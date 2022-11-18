95ee4ced4263f2e351f36533283b05059f51d600
paolamaduro <paolamaduro04@gmail.com>
2022-11-18 20:41:27 +0000

    salvamentodia18

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+tasks = [
+    {'name': 'Estudar', 'finished': False}, 
+    {'name': 'Dormir', 'finished': True}
+]
+
 @app.route('/')
 def home():
     #templates/home.html
-    return render_template('home.html')
+    return render_template('home.html', tasks=tasks)
 
 @app.route('/bye')
 def bye():
